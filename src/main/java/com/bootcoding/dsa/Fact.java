788119c82ffe933c1c12f5e42d41d522464a392c
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-30 10:59:49 +0530

    factorial using recurtion ..

---
+++ src/main/java/com/bootcoding/dsa/Fact.java
@@ -0,0 +1,18 @@
+package com.bootcoding.dsa;
+
+public class Fact
+{
+    public static void main(String[] args) {
+        long res=getFact(5);
+        System.out.println(res);
+    }
+
+    private static long getFact(int n)
+    {
+        if (n==1)
+        {
+            return 1;
+        }
+        return n * getFact(n-1);
+    }
+}
