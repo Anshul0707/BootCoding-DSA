8417ceedadc8fbf7a90dd9b57c185c10d87a4210
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-30 11:00:50 +0530

    Move Zeroes - LeetCode

---
+++ src/main/java/com/bootcoding/dsa/MoveZeroes.java
@@ -0,0 +1,33 @@
+package com.bootcoding.dsa;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MoveZeroes
+{
+    public static void main(String[] args)
+    {
+        MoveZeroes moveZeroes = new MoveZeroes();
+        int nums[]={0,0,0,0,2};
+        moveZeroes.moveZeroes(nums);
+    }
+    public void moveZeroes(int[] nums)
+    {
+        int n = nums.length;
+        int indexCount = 0;
+        for (int index = 0; index < nums.length; index++)
+        {
+           if(nums[index] != 0)
+           {
+               nums[indexCount++] = nums[index];
+           }
+        }
+
+        while(indexCount < n)
+        {
+            nums[indexCount++] = 0;
+        }
+        System.out.println(Arrays.toString(nums));
+
+    }
+}
