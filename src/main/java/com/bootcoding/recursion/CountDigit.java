35ec0faaee6bbf56db4cc65542c99b872b70fdf5
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-30 17:05:38 +0530

    WAP to count the digits of a given number using recursion

---
+++ src/main/java/com/bootcoding/recursion/CountDigit.java
@@ -0,0 +1,21 @@
+package com.bootcoding.recursion;
+
+public class CountDigit
+{
+    public static void main(String[] args)
+    {
+        long n = 345289467;
+        System.out.print("Number of digits : "+ countDigit(n));
+    }
+
+    private static int countDigit(long n)
+    {
+        int count = 0;
+        while (n != 0)
+        {
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+}
