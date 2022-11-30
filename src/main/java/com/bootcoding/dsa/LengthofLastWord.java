27c3c40e05a0248f405875744d2e883f931bc3cc
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-30 11:01:59 +0530

    program to find lenght of last word of given string.

---
+++ src/main/java/com/bootcoding/dsa/LengthofLastWord.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa;
+
+public class LengthofLastWord
+{
+    public static void main(String[] args)
+    {
+        LengthofLastWord lengthofLastWord = new LengthofLastWord();
+        lengthofLastWord.lengthOfLastWord("Hello World");
+    }
+    public int lengthOfLastWord(String s)
+    {
+        String words[] = s.split(" ");
+        String last = words[words.length-1];
+        System.out.println(last);
+        int lastLen = last.length();
+        System.out.println(lastLen);
+
+        return lastLen;
+    }
+}
