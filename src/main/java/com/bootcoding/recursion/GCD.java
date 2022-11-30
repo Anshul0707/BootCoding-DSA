38e8f3eae17134cdbd4fbeb10bbcdef659c3012f
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-30 17:06:30 +0530

    WAP to find GCD of two numbers using recursion

---
+++ src/main/java/com/bootcoding/recursion/GCD.java
@@ -0,0 +1,14 @@
+package com.bootcoding.recursion;
+
+public class GCD
+{
+    public static void main(String[] args)
+    {
+
+    }
+    public static int gcd(int a, int b)
+    {
+
+        return a;
+    }
+}
