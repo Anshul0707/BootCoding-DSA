0a9251cb6f164a80e8e284b7794090a97dafd00b
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-01 13:16:33 +0530

    Make two methods printNaturalNumberForWord and printNaturalNumberBackWord

--- src/main/java/com/bootcoding/recursion/NaturalNumber.java
+++ src/main/java/com/bootcoding/recursion/NaturalNumber.java
@@ -4,16 +4,29 @@ public class NaturalNumber
 {
     public static void main(String[] args) {
         int n=1;
-        System.out.print("First 50 Natural Numbers are:");;
-        PrintNaturalNumber(n);
+        System.out.print("First 50 Natural Numbers are:");
+        printNaturalNumberForWord(n);
+        System.out.println("\n");
+        System.out.print("Last 50 Natural Numbers are:");
+        printNaturalNumberBackWord(n);
     }
 
-    private static void PrintNaturalNumber(int n)
+    private static void printNaturalNumberForWord(int n)
     {
-        if(n<=50)
-        {
-            System.out.print(" "+n+" ");
-            PrintNaturalNumber(n+1);
+        if(n>50){
+            return;
         }
+        System.out.print(" "+n+" ");
+        printNaturalNumberForWord(n+1);
+    }
+
+    private static void printNaturalNumberBackWord(int n)
+    {
+        if(n>50){
+            return;
+        }
+
+        printNaturalNumberBackWord(n+1);
+        System.out.print(" "+n+" ");
     }
 }
