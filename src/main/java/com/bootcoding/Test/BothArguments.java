3391dc47d5cff3cc54f69050f588975496294146
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-09 14:50:26 +0530

    to cheak value of a and b is same or not

---
+++ src/main/java/com/bootcoding/Test/BothArguments.java
@@ -0,0 +1,19 @@
+package com.bootcoding.Test;
+
+public class BothArguments
+{
+    public static void main(String[] args)
+    {
+        boolean res = arguments(2, 8);
+        System.out.println(res);
+    }
+
+    private static boolean arguments(int a, int b)
+    {
+        if (a==10 || b==10|| a+b==10)
+        {
+            return true;
+        }
+        return false;
+    }
+}
