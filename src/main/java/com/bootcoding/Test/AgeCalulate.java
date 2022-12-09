2dd068082b265d3827f1a595c59bcbe5ccba9daf
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-09 14:49:13 +0530

    Age Calculator

---
+++ src/main/java/com/bootcoding/Test/AgeCalulate.java
@@ -0,0 +1,26 @@
+package com.bootcoding.Test;
+
+public class AgeCalulate
+{
+    public static void main(String[] args) {
+        int age=34;
+        int result = ageCulculate(age);
+        System.out.println(result);
+    }
+
+    private static int ageCulculate(int age)
+    {
+        int days = 0;
+        if (age>=0)
+        {
+
+            days=age*365;
+
+        }
+        else
+        {
+            System.out.println("Age should be +ve");
+        }
+        return days;
+    }
+}
