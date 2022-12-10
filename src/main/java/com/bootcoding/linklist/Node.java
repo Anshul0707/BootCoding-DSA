13d41093d89a6614ef86b224cc6f47383baed36e
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-10 16:48:40 +0530

    Node

---
+++ src/main/java/com/bootcoding/linklist/Node.java
@@ -0,0 +1,13 @@
+package com.bootcoding.linklist;
+
+public class Node
+{
+    int data;
+    Node next;
+
+    public Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
