dff4e6ee291133bef6ebd8da163104e938f218d0
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-10 16:45:01 +0530

    Encapsulation

---
+++ src/main/java/com/bootcoding/oops/Encapsulation/Encapsulation.java
@@ -0,0 +1,11 @@
+package com.bootcoding.oops.Encapsulation;
+
+public class Encapsulation
+{
+    public static void main(String[] args)
+    {
+        Person person = new Person();
+        person.setName("Arpit");
+        System.out.println("Name is: " + person.getName());
+    }
+}
