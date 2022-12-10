a5e2202c22a92f1153c21255b2fa5d7fc875acab
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-10 16:45:19 +0530

    Inheritance

---
+++ src/main/java/com/bootcoding/oops/Inheritance/Inheritance.java
@@ -0,0 +1,11 @@
+package com.bootcoding.oops.Inheritance;
+
+public class Inheritance
+{
+    public static void main(String[] args)
+    {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
