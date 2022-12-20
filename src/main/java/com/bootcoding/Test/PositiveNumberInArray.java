dc7a9399c0e02a7262acfa3b2df637567f07803d
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-20 14:44:43 +0530

    positive number array

---
+++ src/main/java/com/bootcoding/Test/PositiveNumberInArray.java
@@ -0,0 +1,19 @@
+package com.bootcoding.Test;
+
+public class PositiveNumberInArray
+{
+    public static void main(String[] args) {
+        int [] arr={1,5,-3,2,11};
+        int res = sumPositive(arr);
+        System.out.println(res);
+    }
+    private static int sumPositive(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            if (num >= 0) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+}
