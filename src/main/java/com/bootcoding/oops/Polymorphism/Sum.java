8c330a9af686625968cc676c0e79786b3e0b5b05
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-20 14:42:01 +0530

    sum using polymorphism

---
+++ src/main/java/com/bootcoding/oops/Polymorphism/Sum.java
@@ -0,0 +1,19 @@
+package com.bootcoding.oops.Polymorphism;
+
+public class Sum
+{
+    public int sum(int x, int y)
+    {
+        return (x + y);
+    }
+
+    public int sum(int x, int y, int z)
+    {
+        return (x + y + z);
+    }
+
+    public double sum(double x, double y)
+    {
+        return (x + y);
+    }
+}
