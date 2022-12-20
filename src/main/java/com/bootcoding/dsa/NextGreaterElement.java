7e01608598cd6c52ab383ba3d8c28efdf6c320fc
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-20 14:40:55 +0530

    Next Greater element

---
+++ src/main/java/com/bootcoding/dsa/NextGreaterElement.java
@@ -0,0 +1,15 @@
+package com.bootcoding.dsa;
+
+public class NextGreaterElement
+{
+    public static void main(String[] args)
+    {
+        int [] arr={23,34,56,12,90,80,40,10};
+        findNGE(arr);
+    }
+
+    private static void findNGE(int [] arr)
+    {
+
+    }
+}
