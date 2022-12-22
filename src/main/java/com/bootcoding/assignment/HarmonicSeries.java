9e6a0f21e4e1259ff330918398900abf46a59f6b
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-22 17:57:34 +0530

    display the n terms of harmonic series and their sum. 1 + 1/2 + 1/3 + 1/4 + 1/5 ... 1/n terms

---
+++ src/main/java/com/bootcoding/assignment/HarmonicSeries.java
@@ -0,0 +1,23 @@
+package com.bootcoding.assignment;
+
+public class HarmonicSeries
+{
+    public static void main(String[] args)
+    {
+        int num=5;
+        harmonicSeries(num);
+    }
+
+    private static double harmonicSeries(int num)
+    {
+        double add = 0.0;
+        for (double i=1;i<=num;i++)
+        {
+//            System.out.println();
+           add=add+1/i;
+        }
+        System.out.println(add);
+        return add;
+    }
+
+}
