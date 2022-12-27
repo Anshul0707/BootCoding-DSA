6753e6d7b8d3ec54f7f7ebee518e9aeb24855719
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-27 15:20:36 +0530

    Selection Sort Example..

---
+++ src/main/java/com/bootcoding/sorting/SelectionSortExample.java
@@ -0,0 +1,33 @@
+package com.bootcoding.sorting;
+
+public class SelectionSortExample
+{
+    public static void main(String[] args)
+    {
+        int [] arr={12, 11, 13, 5, 6};
+        System.out.println();
+        selectionSort(arr);
+        for(int i:arr)
+        {
+            System.out.print(i+" ");
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (arr[j] < arr[index])
+                {
+                    index = j;
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+    }
+}
