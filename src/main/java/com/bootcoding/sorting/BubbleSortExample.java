5c817a5b9641a0c2e9a2ddb2c88474e52d95fa59
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-28 14:47:09 +0530

    code refactor..

--- src/main/java/com/bootcoding/sorting/BubbleSortExample.java
+++ src/main/java/com/bootcoding/sorting/BubbleSortExample.java
@@ -1,10 +1,12 @@
 package com.bootcoding.sorting;
 
 public class BubbleSortExample {
-    public static void main(String[] args) {
-        int[] arr = {12, 11, 13, 5, 6};
+    public static void main(String[] args)
+    {
+        int[] arr = {5,1,7,4};
         bubbleSortExample(arr);
-        for (int i : arr) {
+        for (int i : arr)
+        {
             System.out.print(i + " ");
         }
     }
@@ -14,7 +16,7 @@ public class BubbleSortExample {
         int temp = 0;
         for (int i = 0; i < arr.length; i++)
         {
-            for (int j = 1; j < arr.length-1; j++)
+            for (int j = 1; j < arr.length; j++)
             {
                 if (arr[j - 1] > arr[j])
                 {
