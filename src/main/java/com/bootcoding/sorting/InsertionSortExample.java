773c655d9d473261e8695e894104406dc0165340
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-28 14:15:55 +0530

    Insertion Sort Example

---
+++ src/main/java/com/bootcoding/sorting/InsertionSortExample.java
@@ -0,0 +1,19 @@
+package com.bootcoding.sorting;
+
+public class InsertionSortExample
+{
+    public static void main(String[] args)
+    {
+        int [] arr={12, 11, 13, 5, 6};
+        selectionSort(arr);
+        for(int i:arr)
+        {
+            System.out.print(i+" ");
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+
+    }
+}
