1fb135a75733044a6bf7adcc008ea6b9f1d00dd8
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-03 16:07:33 +0530

    hashmap Example

---
+++ src/main/java/com/bootcoding/hashmap/Example.java
@@ -0,0 +1,25 @@
+package com.bootcoding.hashmap;
+
+import java.util.HashMap;
+
+public class Example
+{
+    public static void main(String[] args)
+    {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("Anshul",07);
+        map.put("Min",13);
+        map.put("Harshal",06);
+        map.put("Suvi",19);
+
+        System.out.println("Size of map: "+map.size());
+        System.out.println(map);
+
+        if (map.containsKey("Suvi"))
+        {
+            Integer anshul = map.get("Anshul");
+            System.out.println("value of anshul is "+anshul);
+        }
+    }
+}
