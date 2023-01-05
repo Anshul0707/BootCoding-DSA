3c5c9516f671d7613d422647e10bb136e5438b50
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-05 11:19:44 +0530

    ..

---
+++ src/main/java/com/bootcoding/assignment/GradeSystem.java
@@ -0,0 +1,18 @@
+package com.bootcoding.assignment;
+
+public class GradeSystem
+{
+    public static void main(String[] args) {
+        int arr[]={1,5,6,4,2,8};
+//        calGrade(arr);
+    }
+
+    public char[]  calGrade(int [] studentPercentage)
+    {
+        for (int i=0; i<studentPercentage.length; i++)
+        {
+
+        }
+        return new char[0];
+    }
+}
