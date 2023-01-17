83e82ac7a93e45dcb2bc057565a82686d907d5d9
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-17 15:03:58 +0530

    ..

---
+++ src/main/java/com/bootcoding/recursion/NumberofDigits.java
@@ -0,0 +1,22 @@
+package com.bootcoding.recursion;
+
+public class NumberofDigits
+{
+    public static void main(String[] args)
+    {
+        int num=234567;
+        System.out.println("Number of Digits:- "+DigitsCount(num));
+    }
+
+    static int Count = 0;
+    public static int DigitsCount(int Number)
+    {
+        if(Number > 0)
+        {
+            Count = Count + 1;
+            DigitsCount(Number / 10);
+        }
+        return Count;
+    }
+
+}
