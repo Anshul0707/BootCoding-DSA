c3394e7c7020b220d0ddfc4d126cf70f81bcc079
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-23 16:14:03 +0530

    LeetCode- Reverse String

---
+++ src/main/java/com/bootcoding/leetcode/ReverseString.java
@@ -0,0 +1,13 @@
+package com.bootcoding.leetcode;
+
+public class ReverseString
+{
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for(int i=0;i<n/2;i++){
+            s[i] = (char)(s[i]+s[n-i-1]);
+            s[n-i-1] = (char)(s[i]-s[n-i-1]);
+            s[i] = (char)(s[i]-s[n-i-1]);
+        }
+    }
+}
