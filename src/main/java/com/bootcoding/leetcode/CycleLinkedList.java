edb61f7bfc9d8a9f0f44c7534cd141da93cd791c
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-23 16:04:01 +0530

    LeetCode- Cycle Linked List

---
+++ src/main/java/com/bootcoding/leetcode/CycleLinkedList.java
@@ -0,0 +1,38 @@
+package com.bootcoding.leetcode;
+
+public class CycleLinkedList
+{
+    class ListNode
+    {
+     int val;
+      ListNode next;
+      ListNode(int x) {
+         val = x;
+          next = null;
+      }
+    }
+
+    public boolean hasCycle(ListNode head)
+    {
+        ListNode sp=head;
+        ListNode fp=head;
+
+        while(true)
+        {
+
+            if (fp == null || fp.next==null)
+            {
+                return false;
+            }
+            sp=sp.next;
+            fp=fp.next.next;
+
+            if (sp==fp)
+            {
+                return true;
+            }
+
+        }
+
+    }
+}
