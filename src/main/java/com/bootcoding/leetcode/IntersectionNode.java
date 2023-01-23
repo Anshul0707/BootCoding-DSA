9a1bee89c82670084d8c43aab4a7ebf30e148187
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-23 16:00:53 +0530

    LeetCode-Intersection Of Two linked List..

---
+++ src/main/java/com/bootcoding/leetcode/IntersectionNode.java
@@ -0,0 +1,32 @@
+package com.bootcoding.leetcode;
+
+public class IntersectionNode
+{
+    public class ListNode
+    {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB)
+    {
+        ListNode temp1=headA;
+        while (temp1 != null)
+        {
+            ListNode temp2=headB;
+            while (temp2 != null)
+            {
+                if(temp1==temp2)
+                {
+                    return temp1;
+                }
+                temp2= temp2.next;
+            }
+            temp1 = temp1.next;
+        }
+        return null;
+    }
+}
