bf1fc274b82c766480f3a3219eae4969cdb65fdd
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-23 16:11:39 +0530

    LeetCode- number of steps to reduce a number to zero

---
+++ src/main/java/com/bootcoding/leetcode/NumberOfSteps.java
@@ -0,0 +1,23 @@
+package com.bootcoding.leetcode;
+
+public class NumberOfSteps
+{
+    public int numberOfSteps(int num)
+    {
+        int count=0;
+        while(num!=0)
+        {
+            if(num%2==0)
+            {
+                num=num/2;
+                count++;
+            }
+            else
+            {
+                num=num-1;
+                count++;
+            }
+        }
+        return count;
+    }
+}
