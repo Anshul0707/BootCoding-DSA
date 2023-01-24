69e94d229e0ba1a27148456311e1ac14fbf50951
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-24 15:06:58 +0530

    LeetCode- lengthOfLastWord

--- src/main/java/com/bootcoding/leetcode/LengthOfLastWord.java
+++ src/main/java/com/bootcoding/leetcode/LengthOfLastWord.java
@@ -1,6 +1,12 @@
 package com.bootcoding.leetcode;
 
 public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+        int res = lengthOfLastWord.lengthOfLastWord(s);
+        System.out.println(res);
+    }
     public int lengthOfLastWord(String s)
     {
         String words[] = s.split(" ");
