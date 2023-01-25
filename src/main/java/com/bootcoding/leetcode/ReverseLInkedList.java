6f6d0a5eda811a4a50b33052288cd19a63a0af3a
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-25 11:00:55 +0530

    code implement.

--- src/main/java/com/bootcoding/leetcode/ReverseLInkedList.java
+++ src/main/java/com/bootcoding/leetcode/ReverseLInkedList.java
@@ -10,7 +10,8 @@ public class ReverseLInkedList
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList(ListNode head)
+    {
         ListNode current=head;
         ListNode previous=null;
         ListNode next=null;
