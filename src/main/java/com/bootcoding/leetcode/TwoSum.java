d0d440386911289a4657f84f93d39d4d31e627e5
Anshul0707 <anshul.markwade786@gmail.com>
2023-02-06 14:14:41 +0530

    code implemented

--- src/main/java/com/bootcoding/leetcode/TwoSum.java
+++ src/main/java/com/bootcoding/leetcode/TwoSum.java
@@ -1,5 +1,8 @@
 package com.bootcoding.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum
 {
     public int[] twoSum(int[] nums, int target)
@@ -15,5 +18,23 @@ public class TwoSum
             }
         }
         return new int[] {};
+    }
+    public int[] twoSum2(int[] nums, int target)
+    {
+        Map<Integer, Integer> cache = new HashMap<>();
+        int[] aa = new int[2];
+        for (int i = 0; i < nums.length; i++)
+        {
+            if(cache.containsKey(nums[i]))
+            {
+                aa[0] = cache.get(aa[i]);
+                aa[1]=i;
+            }
+            else
+            {
+                cache.put(target-aa[i],i);
+            }
+        }
+        return aa;
     }
 }
