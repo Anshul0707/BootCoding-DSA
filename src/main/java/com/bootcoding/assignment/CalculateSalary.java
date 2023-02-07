33598303db7186d735ac3072269196147455bae6
Anshul0707 <anshul.markwade786@gmail.com>
2023-02-07 23:58:51 +0530

    code implemented

--- src/main/java/com/bootcoding/assignment/CalculateSalary.java
+++ src/main/java/com/bootcoding/assignment/CalculateSalary.java
@@ -17,9 +17,6 @@ public class CalculateSalary
         System.out.println("Salary amount/hr: ");
         int Salary = s.nextInt();
 
-//        Employees ID = 0342
-//        Salary = U$ 120000.00
-
         System.out.println("Employees ID "+ID);
         System.out.println("Salary ="+"U$ "+hrs*Salary);
 
