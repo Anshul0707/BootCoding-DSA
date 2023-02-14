0374a87e9d035af126b2b2f135e3bff57baf5213
Anshul0707 <anshul.markwade786@gmail.com>
2023-02-14 10:54:54 +0530

    basic changes

--- src/main/java/com/bootcoding/assignment/MultiplicationTable.java
+++ src/main/java/com/bootcoding/assignment/MultiplicationTable.java
@@ -4,15 +4,15 @@ public class MultiplicationTable
 {
     public static void main(String[] args)
     {
-        int n=8;
-        multiplicationTable(n);
+        int num=8;
+        multiplicationTable(num);
     }
 
-    private static void multiplicationTable(int n)
+    private static void multiplicationTable(int num)
     {
         for (int i=1;i<=10;i++)
         {
-            System.out.println(n+" x "+i+" = "+n*i);
+            System.out.println(num+" x "+i+" = "+num*i);
         }
     }
 }
