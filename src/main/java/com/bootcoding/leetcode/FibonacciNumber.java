682e4d3eec8a9ce62c4eee29ffeefafea39f4301
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-03 13:58:06 +0530

    basic changes

--- src/main/java/com/bootcoding/leetcode/FibonacciNumber.java
+++ src/main/java/com/bootcoding/leetcode/FibonacciNumber.java
@@ -3,12 +3,10 @@ package com.bootcoding.leetcode;
 public class FibonacciNumber {
     public int fib(int n)
     {
-        if(n==0)
-        {
+        if(n==0) {
             return 0;
         }
-        if(n==1)
-        {
+        if(n==1) {
             return 1;
         }
         return fib(n-1)+fib(n-2);
