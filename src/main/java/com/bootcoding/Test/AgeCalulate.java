026d8a005e31e6736dc990a602a8c17bca6512d0
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-04 22:38:51 +0530

    basic changes

--- src/main/java/com/bootcoding/Test/AgeCalulate.java
+++ src/main/java/com/bootcoding/Test/AgeCalulate.java
@@ -11,14 +11,10 @@ public class AgeCalulate
     private static int ageCulculate(int age)
     {
         int days = 0;
-        if (age>=0)
-        {
-
+        if (age>=0) {
             days=age*365;
-
         }
-        else
-        {
+        else {
             System.out.println("Age should be +ve");
         }
         return days;
