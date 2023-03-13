37c57aed73da3f4529dc615e564760f14318d078
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-13 23:17:52 +0530

    basic changes

--- src/main/java/com/bootcoding/recursion/PrintArray.java
+++ src/main/java/com/bootcoding/recursion/PrintArray.java
@@ -2,20 +2,19 @@ package com.bootcoding.recursion;
 
 public class PrintArray
 {
-    public static void main(String[] args)
-    {
-        int[] arr = {2, 4, 6, 8, 10};
-        printForword(arr, 0);
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10,8};
+        printForward(arr, 0);
         printReverse(arr,0);
     }
 
-    public static void printForword(int[] arr, int index) {
+    public static void printForward(int[] arr, int index) {
         if (index == arr.length)
         {
             return;
         }
         System.out.println(arr[index]);
-        printForword(arr, index + 1);
+        printForward(arr, index + 1);
     }
 
     public static void printReverse(int[] arr, int index) {
