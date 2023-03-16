2ca23f8ec781bc092274f6d14bb72c28779e18fd
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-16 11:19:45 +0530

    basic changes

--- src/main/java/com/bootcoding/assignment/CalculatesNumbersOfNotes.java
+++ src/main/java/com/bootcoding/assignment/CalculatesNumbersOfNotes.java
@@ -9,6 +9,7 @@ public class CalculatesNumbersOfNotes
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Input the amount: ");
+
         int amount = sc.nextInt();
 
         int [] notes={2000,500,200,100,50,20,10,5,2,1};
