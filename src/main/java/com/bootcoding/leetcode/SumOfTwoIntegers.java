2fc18c6961c125d03dee1672506279e80922cb9f
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-24 15:47:18 +0530

    SumOfTwoIntegers

---
+++ src/main/java/com/bootcoding/leetcode/SumOfTwoIntegers.java
@@ -0,0 +1,11 @@
+package com.bootcoding.leetcode;
+
+public class SumOfTwoIntegers {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        System.out.println(getSum(a,b));
+    }
+        public static int getSum(int a, int b) {
+            return Integer.sum(a, b);
+        }
+}
