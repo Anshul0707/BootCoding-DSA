a649f4dce1ef7096b8b8d440ad7ae7b1824af244
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-24 16:09:00 +0530

    FindMinimumInRotatedSortedArrayII

---
+++ src/main/java/com/bootcoding/leetcode/FindMinimumInRotatedSortedArrayII.java
@@ -0,0 +1,24 @@
+package com.bootcoding.leetcode;
+
+import java.util.Arrays;
+
+public class FindMinimumInRotatedSortedArrayII
+{
+    public static void main(String[] args) {
+
+    }
+//    public static int findMin(int[] nums) {
+//        int min = nums[0];
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] < min) {
+//                min = nums[i];
+//            }
+//        }
+//        return min;
+//    }
+
+    public static int findMin(int[] nums) {
+        Arrays.sort(nums);
+        return nums[0];
+    }
+}
