ff24bd8678e361995c8239def7bcb45ca5d17e10
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-24 15:44:38 +0530

    Add another method

--- src/main/java/com/bootcoding/leetcode/BinarySearch.java
+++ src/main/java/com/bootcoding/leetcode/BinarySearch.java
@@ -23,4 +23,16 @@ public class BinarySearch
             return binarySearch(a, x, left, mid-1);
         }
     }
+
+
+//        public int search(int[] nums, int target) {
+//            int res = -1;
+//            for(int i=0; i<nums.length; i++){
+//                if(target == nums[i]){
+//                    res = i;
+//                }
+//            }
+//            return res;
+//        }
+
 }
