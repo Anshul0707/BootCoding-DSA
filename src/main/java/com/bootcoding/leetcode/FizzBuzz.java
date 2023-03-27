0fa94b5596a5a85daab17424b2417bf67b7fbd09
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-27 13:23:49 +0530

    ..

--- src/main/java/com/bootcoding/leetcode/FizzBuzz.java
+++ src/main/java/com/bootcoding/leetcode/FizzBuzz.java
@@ -7,7 +7,8 @@ public class FizzBuzz
 {
     public List<String> fizzBuzz(int n) {
         ArrayList<String> str=new ArrayList<>();
-        for(int i=1;i<=n;i++) {
+        for(int i=1;i<=n;i++)
+        {
             if(i%3==0 && i%5==0)
             {
                 str.add("FizzBuzz");
