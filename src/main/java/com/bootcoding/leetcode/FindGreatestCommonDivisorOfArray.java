c46387b62753f7d1a98825b2542e0a538a40a5bc
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-11 16:19:09 +0530

    FindGreatestCommonDivisorOfArray

--- src/main/java/com/bootcoding/leetcode/FindGreatestCommonDivisorOfArray.java
+++ src/main/java/com/bootcoding/leetcode/FindGreatestCommonDivisorOfArray.java
@@ -5,15 +5,19 @@ import java.util.Arrays;
 public class FindGreatestCommonDivisorOfArray
 {
     public static void main(String[] args) {
-        int[] nums={2,1,5,6,9,10};
-        findGCD(nums);
-
+        int[] nums={2,5,6,9,10};
+        System.out.println(findGCD(nums));
     }
 
     private static int findGCD(int[] nums) {
         Arrays.sort(nums);
+        int result = 0;
         int min=nums[0] , max=nums[nums.length-1];
-
-        return 0;
+        for (int i = 1; i < max; i++) {
+            if (min%i==0 && max%i==0) {
+                result=i;
+            }
+        }
+        return result;
     }
 }
