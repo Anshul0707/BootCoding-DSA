cf25d986512edf420f14c55f1de8e2b6f4f6353c
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-11 16:19:48 +0530

    FindGreatestCommonDivisorOfArray

--- src/main/java/com/bootcoding/leetcode/FindGreatestCommonDivisorOfArray.java
+++ src/main/java/com/bootcoding/leetcode/FindGreatestCommonDivisorOfArray.java
@@ -13,7 +13,7 @@ public class FindGreatestCommonDivisorOfArray
         Arrays.sort(nums);
         int result = 0;
         int min=nums[0] , max=nums[nums.length-1];
-        for (int i = 1; i < max; i++) {
+        for (int i = 1; i <= max; i++) {
             if (min%i==0 && max%i==0) {
                 result=i;
             }
