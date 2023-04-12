848df455ab398aa5a9f52f1289b9279f788f4826
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-12 14:50:09 +0530

    Leetcode

--- src/main/java/com/bootcoding/leetcode/BuildArrayFromPermutation.java
+++ src/main/java/com/bootcoding/leetcode/BuildArrayFromPermutation.java
@@ -12,7 +12,7 @@ public class BuildArrayFromPermutation {
     public static int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-
+            ans[i] = nums[nums[i]];
         }
         return ans;
     }
