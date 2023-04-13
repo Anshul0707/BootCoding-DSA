68b5edd7bb4613ec4c9e83827e9e80d915f7587c
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-13 11:29:15 +0530

    updated code..

--- src/main/java/com/bootcoding/leetcode/MaximumProductOfThreeNumbers.java
+++ src/main/java/com/bootcoding/leetcode/MaximumProductOfThreeNumbers.java
@@ -11,26 +11,11 @@ public class MaximumProductOfThreeNumbers
     }
 
     private static int maximumProduct(int[] nums) {
-        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
-        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
-        for (int n: nums) {
-            if (n <= min1) {
-                min2 = min1;
-                min1 = n;
-            } else if (n <= min2) {     // n lies between min1 and min2
-                min2 = n;
-            }
-            if (n >= max1) {            // n is greater than max1, max2 and max3
-                max3 = max2;
-                max2 = max1;
-                max1 = n;
-            } else if (n >= max2) {     // n lies betweeen max1 and max2
-                max3 = max2;
-                max2 = n;
-            } else if (n >= max3) {     // n lies betwen max2 and max3
-                max3 = n;
-            }
-        }
-        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+        Arrays.sort(nums);
+        int n = nums.length;
+        int product1 = nums[n-1] * nums[n-2] * nums[n-3]; // product of three largest numbers
+        int product2 = nums[0] * nums[1] * nums[n-1]; // product of two smallest numbers and largest number
+
+        return Math.max(product1, product2);
     }
 }
