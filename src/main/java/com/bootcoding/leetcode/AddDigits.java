3b669201e9f6763c9f9f2d7b6dd5cf58d50953ed
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-14 23:21:40 +0530

    leetcode.

--- src/main/java/com/bootcoding/leetcode/AddDigits.java
+++ src/main/java/com/bootcoding/leetcode/AddDigits.java
@@ -8,7 +8,12 @@ public class AddDigits
     }
 
     public static int addDigits(int num) {
-
-        return num;
+        if (num == 0) {
+            return 0;
+        } else if (num % 9 == 0) {
+            return 9;
+        } else {
+            return num % 9;
+        }
     }
 }
