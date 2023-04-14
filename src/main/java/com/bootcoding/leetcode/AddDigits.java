fcbae8e9286249161d23fc568859a52da331d4e2
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-14 23:21:12 +0530

    leetcode.

---
+++ src/main/java/com/bootcoding/leetcode/AddDigits.java
@@ -0,0 +1,14 @@
+package com.bootcoding.leetcode;
+
+public class AddDigits
+{
+    public static void main(String[] args) {
+        int num=15;
+        System.out.println(addDigits(num));
+    }
+
+    public static int addDigits(int num) {
+
+        return num;
+    }
+}
