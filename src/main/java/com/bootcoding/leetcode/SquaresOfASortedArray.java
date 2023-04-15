b2bc746a149e521eb34036de89d14ccae140caf8
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-15 12:15:42 +0530

    SquaresOfASortedArray

---
+++ src/main/java/com/bootcoding/leetcode/SquaresOfASortedArray.java
@@ -0,0 +1,20 @@
+package com.bootcoding.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class SquaresOfASortedArray
+{
+    public static void main(String[] args) {
+        int[] nums={-4,-1,0,3,10};
+        int[] ints = sortedSquares(nums);
+        for (int res : ints) {
+            System.out.println(res);
+        }
+    }
+
+    private static int[] sortedSquares(int[] nums) {
+
+        return nums;
+    }
+}
