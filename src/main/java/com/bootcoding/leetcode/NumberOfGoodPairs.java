270309b23f0f9b6a57bdf13f83bf56d5992f05ab
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-17 15:22:49 +0530

    NumberOfGoodPairs

---
+++ src/main/java/com/bootcoding/leetcode/NumberOfGoodPairs.java
@@ -0,0 +1,15 @@
+package com.bootcoding.leetcode;
+
+public class NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int [] nums={1,2,3,1,1,3};
+        int result = numIdenticalPairs(nums);
+        System.out.println(result);
+
+    }
+
+    private static int numIdenticalPairs(int[] nums) {
+
+        return count;
+    }
+}
