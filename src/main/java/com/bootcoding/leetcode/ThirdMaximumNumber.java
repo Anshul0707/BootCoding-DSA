26242cbc663aae03289306ec0b3ca81296b60cab
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-18 11:31:46 +0530

    ThirdMaximumNumber

--- src/main/java/com/bootcoding/leetcode/ThirdMaximumNumber.java
+++ src/main/java/com/bootcoding/leetcode/ThirdMaximumNumber.java
@@ -1,4 +1,16 @@
 package com.bootcoding.leetcode;
 
+import java.util.Arrays;
+
 public class ThirdMaximumNumber {
+    public static void main(String[] args) {
+        int[] nums={2,2,3,1};
+        int res = thirdMax(nums);
+        System.out.println(res);
+    }
+
+    private static int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-3];
+    }
 }
