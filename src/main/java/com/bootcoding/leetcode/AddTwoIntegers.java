5085e931f27a462946131126df18813f60bba052
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-20 10:37:30 +0530

    AddTwoIntegers

---
+++ src/main/java/com/bootcoding/leetcode/AddTwoIntegers.java
@@ -0,0 +1,15 @@
+package com.bootcoding.leetcode;
+
+public class AddTwoIntegers
+{
+    public static void main(String[] args) {
+        int num1=12;
+        int num2=5;
+        int sum = sum(num1, num2);
+        System.out.println(sum);
+    }
+
+    private static int sum(int num1, int num2) {
+        return 0;
+    }
+}
