fbdf8f1c179678188e5d6ef648eb30991a1a6889
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-21 13:50:44 +0530

    CheckIfTheSentenceIsPangram

---
+++ src/main/java/com/bootcoding/leetcode/CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,17 @@
+package com.bootcoding.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CheckIfTheSentenceIsPangram {
+    public static void main(String[] args) {
+        String sentence="thequickbrownfoxjumpsoverthelazydog";
+        boolean b = checkIfPangram(sentence);
+        System.out.println(b);
+    }
+
+    private static boolean checkIfPangram(String sentence) {
+
+        return false;
+    }
+}
