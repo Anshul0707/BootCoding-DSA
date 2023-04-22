3fd6944a15f5329181631826d1f5e6fa7a2c4e23
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-22 12:52:57 +0530

    MinimumOperationsToMakeTheArrayIncreasing

--- src/main/java/com/bootcoding/leetcode/MinimumOperationsToMakeTheArrayIncreasing.java
+++ src/main/java/com/bootcoding/leetcode/MinimumOperationsToMakeTheArrayIncreasing.java
@@ -3,6 +3,14 @@ package com.bootcoding.leetcode;
 public class MinimumOperationsToMakeTheArrayIncreasing
 {
     public static void main(String[] args) {
+        int[] nums={1,1,1};
+        int res = minOperations(nums);
+        System.out.println(res);
+    }
 
+    private static int minOperations(int[] nums) {
+        int count=0;
+
+        return count;
     }
 }
