30a9ee282a9c8df2121d0f8c3be8c4d3726b6c33
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-22 12:53:17 +0530

    MinimumOperationsToMakeTheArrayIncreasing

--- src/main/java/com/bootcoding/leetcode/MinimumOperationsToMakeTheArrayIncreasing.java
+++ src/main/java/com/bootcoding/leetcode/MinimumOperationsToMakeTheArrayIncreasing.java
@@ -10,7 +10,13 @@ public class MinimumOperationsToMakeTheArrayIncreasing
 
     private static int minOperations(int[] nums) {
         int count=0;
-
+        for (int i = 1; i <nums.length; i++) {
+            if (nums[i]<= nums[i-1]){
+              int diff=nums[i-1]-nums[i]+1;
+              nums[i]=nums[i]+diff;
+              count=count+diff;
+            }
+        }
         return count;
     }
 }
