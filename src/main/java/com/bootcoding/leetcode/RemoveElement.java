24b64e8582368c55a71ea71cb949aab693a757e1
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-23 15:08:37 +0530

    RemoveElement

--- src/main/java/com/bootcoding/leetcode/RemoveElement.java
+++ src/main/java/com/bootcoding/leetcode/RemoveElement.java
@@ -2,6 +2,12 @@ package com.bootcoding.leetcode;
 
 public class RemoveElement {
     public static void main(String[] args) {
+        int[] nums={3,2,2,3};
+        int val=3;
+        removeElement(nums,val);
+    }
 
+    private static int removeElement(int[] nums, int val) {
+        return 0;
     }
 }
