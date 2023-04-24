1dd0ec5b8aeefae01f69b516900f14806ff47f35
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-24 12:49:47 +0530

    TruncateSentence

--- src/main/java/com/bootcoding/leetcode/TruncateSentence.java
+++ src/main/java/com/bootcoding/leetcode/TruncateSentence.java
@@ -1,4 +1,24 @@
 package com.bootcoding.leetcode;
 
 public class TruncateSentence {
+    public static void main(String[] args) {
+        String s="Hello how are you Contestant";
+        int k=4;
+        String s1 = truncateSentence(s, k);
+        System.out.println(s1);
+    }
+
+    public static String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            sb.append(words[i]);
+            sb.append(" ");
+            if (i != k - 1) {
+                sb.append("");
+            }
+        }
+        return sb.toString().trim();
+    }
+
 }
