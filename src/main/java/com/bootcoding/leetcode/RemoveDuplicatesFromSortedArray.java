9c446fe8fc97d9b2a4de593b878a0a229d1218dc
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-24 12:56:03 +0530

    RemoveDuplicatesFromSortedArray

--- src/main/java/com/bootcoding/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/bootcoding/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -1,7 +1,5 @@
 package com.bootcoding.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
 
 public class RemoveDuplicatesFromSortedArray
 {
@@ -11,10 +9,16 @@ public class RemoveDuplicatesFromSortedArray
         System.out.println(res);
     }
     private static int removeDuplicates(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int rr:nums ) {
-            set.add(rr);
+        if (nums.length == 0) {
+            return 0;
         }
-        return set.size();
+        int i = 0; // pointer to keep track of the non-duplicate element
+        for (int j = 1; j < nums.length; j++) { // pointer to traverse the array
+            if (nums[j] != nums[i]) {
+                i++; // increment i when a new non-duplicate element is found
+                nums[i] = nums[j]; // update the non-duplicate element with the value of the new element
+            }
+        }
+        return i + 1; // return the length of the non-duplicate array
     }
 }
