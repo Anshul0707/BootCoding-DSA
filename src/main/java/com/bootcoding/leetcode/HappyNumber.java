eb56e7890f299d08ec06e5915ae58f69ac2a618e
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-27 14:37:56 +0530

    HappyNumber

---
+++ src/main/java/com/bootcoding/leetcode/HappyNumber.java
@@ -0,0 +1,15 @@
+package com.bootcoding.leetcode;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        int n=19;
+        boolean result = isHappy(n);
+        System.out.println(result);
+    }
+
+    private static boolean isHappy(int n) {
+        int sum=1;
+
+        return false;
+    }
+}
