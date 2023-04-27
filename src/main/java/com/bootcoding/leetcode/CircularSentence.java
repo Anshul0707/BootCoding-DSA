af8aef95f162b98dce6cf705d2a67d8859181065
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-27 14:37:02 +0530

    CircularSentence

--- src/main/java/com/bootcoding/leetcode/CircularSentence.java
+++ src/main/java/com/bootcoding/leetcode/CircularSentence.java
@@ -8,18 +8,25 @@ public class CircularSentence
         System.out.println(circularSentence);
     }
 
-    private static boolean isCircularSentence(String sentence) {
-        int count = 0;
-        String[] array = sentence.split(" ");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                char lastLetter = array[i].charAt(array[i].length() - 1); // use array[i] instead of sentence
-                char firstLetter = array[j].charAt(0);
-                if (lastLetter == firstLetter) {
-                    count++;
-                }
+    public static boolean isCircularSentence(String sentence) {
+        String[] words = sentence.split(" ");
+
+        if (words.length==1 && sentence.charAt(0) == sentence.charAt(sentence.length()-1)){
+            return true;
+        }
+
+        if (words.length==1 && sentence.charAt(0)!=sentence.charAt(sentence.length()-1)){
+            return false;
+        }
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+            if (!Character.isLetter(ch) && sentence.charAt(i-1)!=sentence.charAt(i+1) || sentence.charAt(0)!=sentence.charAt(sentence.length()-1)){
+               return false;
             }
         }
-        return count == array.length - 1; // simplify the return statement
+
+        return true;
     }
+
 }
