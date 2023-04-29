1df66ae35802bb99c4dbbc416212edad25627e75
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-29 11:32:05 +0530

    CountTheNumberOfVowelStringsInRange

--- src/main/java/com/bootcoding/leetcode/CountTheNumberOfVowelStringsInRange.java
+++ src/main/java/com/bootcoding/leetcode/CountTheNumberOfVowelStringsInRange.java
@@ -12,7 +12,14 @@ public class CountTheNumberOfVowelStringsInRange
 
     public static int vowelStrings(String[] words, int left, int right) {
         int count = 0;
-
+        for (int i = left; i <= right; i++) {
+            String word = words[i];
+            char first = word.charAt(0);
+            char last = word.charAt(word.length() - 1);
+            if (isVowel(first) && isVowel(last)) {
+                count++;
+            }
+        }
         return count;
     }
 
