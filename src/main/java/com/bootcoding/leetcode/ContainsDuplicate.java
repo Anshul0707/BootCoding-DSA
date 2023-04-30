59ff14b2f207153266606fa3c889ba15746f5cf8
Anshul0707 <anshul.markwade786@gmail.com>
2023-04-30 12:01:12 +0530

    ContainsDuplicate

---
+++ src/main/java/com/bootcoding/leetcode/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+package com.bootcoding.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        return true;
+    }
+}
