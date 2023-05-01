522391acec3e45b98b72cfebaccbee80a5f9b345
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-01 22:58:36 +0530

    AverageSalaryExcludingTheMinimumAndMaximumSalary

--- src/main/java/com/bootcoding/leetcode/AverageSalaryExcludingTheMinimumAndMaximumSalary.java
+++ src/main/java/com/bootcoding/leetcode/AverageSalaryExcludingTheMinimumAndMaximumSalary.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 public class AverageSalaryExcludingTheMinimumAndMaximumSalary
 {
     public static void main(String[] args) {
-        int[] salary={4000,3000,1000,2000};
+        int[] salary={48000,59000,99000,13000,78000,45000,31000,17000,39000,37000,93000,77000,33000,28000,4000,54000,67000,6000,1000,11000};
         double res = average(salary);
         System.out.println(res);
     }
 
     private static double average(int[] salary) {
         Arrays.sort(salary);
-        int sum=0;
-        for (int i = 1; i < salary.length - 2; i++) {
+        int sum=0,count=0;
+        for (int i = 1; i < salary.length - 1; i++) {
         sum = sum + salary[i];
+        count++;
         }
-        int hh = (salary.length) - 2;
-        return sum/hh;
+        return sum/count;
     }
 }
