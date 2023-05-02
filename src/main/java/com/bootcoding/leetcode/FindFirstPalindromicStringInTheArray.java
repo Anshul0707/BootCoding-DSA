b4616572decc43fbd7fc03ae3a5477195f44ef22
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-02 13:51:48 +0530

    FindFirstPalindromicStringInTheArray

--- src/main/java/com/bootcoding/leetcode/FindFirstPalindromicStringInTheArray.java
+++ src/main/java/com/bootcoding/leetcode/FindFirstPalindromicStringInTheArray.java
@@ -1,4 +1,20 @@
 package com.bootcoding.leetcode;
 
 public class FindFirstPalindromicStringInTheArray {
+    public static void main(String[] args) {
+        String[] words={"abc","car","ada","racecar","cool"};
+        String result = firstPalindrome(words);
+        System.out.println(result);
+    }
+
+    private static String firstPalindrome(String[] words) {
+        for (String originalString : words) {
+            StringBuffer sb = new StringBuffer(originalString);
+            String revString = sb.reverse().toString();
+            if (originalString.equals(revString)) {
+                return originalString;
+            }
+        }
+        return "";
+    }
 }
