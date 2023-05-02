5e957816000bbfe7956c24867e648c20dc761e4b
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-02 13:53:22 +0530

    ReverseInteger

--- src/main/java/com/bootcoding/leetcode/ReverseInteger.java
+++ src/main/java/com/bootcoding/leetcode/ReverseInteger.java
@@ -1,4 +1,12 @@
 package com.bootcoding.leetcode;
 
 public class ReverseInteger {
+    public static void main(String[] args) {
+        int x=123;
+        reverse(x);
+    }
+
+    private static int reverse(int x) {
+        return 0;
+    }
 }
