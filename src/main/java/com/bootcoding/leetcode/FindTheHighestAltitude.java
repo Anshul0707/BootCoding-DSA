437e0b671e4d33acb3c9fc7d600f7df537e958f8
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-03 12:44:23 +0530

    FindTheHighestAltitude

---
+++ src/main/java/com/bootcoding/leetcode/FindTheHighestAltitude.java
@@ -0,0 +1,13 @@
+package com.bootcoding.leetcode;
+
+public class FindTheHighestAltitude
+{
+    public static void main(String[] args) {
+        int [] gain={-5,1,5,0,-7};
+        largestAltitude(gain);
+    }
+
+    private static int largestAltitude(int[] gain) {
+        return 0;
+    }
+}
