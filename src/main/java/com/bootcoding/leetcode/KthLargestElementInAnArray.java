2c532bd8257ae95ff31799b392a3946915c580c8
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-04 14:15:43 +0530

    KthLargestElementInAnArray

--- src/main/java/com/bootcoding/leetcode/KthLargestElementInAnArray.java
+++ src/main/java/com/bootcoding/leetcode/KthLargestElementInAnArray.java
@@ -1,14 +1,18 @@
 package com.bootcoding.leetcode;
 
+import java.util.Arrays;
+
 public class KthLargestElementInAnArray {
     public static void main(String[] args) {
-        int[] nums={3,2,1,5,6,4};
-        int k=2;
-        findKthLargest(nums,k);
+        int[] nums={3,2,3,1,2,4,5,5,6};
+        int k=4;
+        int kthLargest = findKthLargest(nums, k);
+        System.out.println(kthLargest);
     }
 
     private static int findKthLargest(int[] nums, int k) {
-
-        return k;
+        int n = nums.length;
+        Arrays.sort(nums);
+        return nums[n-k];
     }
 }
