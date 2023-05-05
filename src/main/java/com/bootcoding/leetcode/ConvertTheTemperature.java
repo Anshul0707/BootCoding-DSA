eef360e0a962730744e8384347ed56839a8178f2
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-05 13:45:04 +0530

    convertTemperature

--- src/main/java/com/bootcoding/leetcode/ConvertTheTemperature.java
+++ src/main/java/com/bootcoding/leetcode/ConvertTheTemperature.java
@@ -11,7 +11,8 @@ public class ConvertTheTemperature {
 
     private static double[] convertTemperature(double celsius) {
         double[] res = new double[2];
-
+        res[0]=celsius + 273.15;
+        res[1]=celsius  * 1.80 + 32.00;
         return res;
     }
 }
