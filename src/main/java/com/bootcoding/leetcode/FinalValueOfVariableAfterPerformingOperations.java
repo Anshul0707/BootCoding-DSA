17c2207d783237c4ea149b36c12511e2c75da31e
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-05 13:45:51 +0530

    FinalValueOfVariableAfterPerformingOperations

---
+++ src/main/java/com/bootcoding/leetcode/FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,15 @@
+package com.bootcoding.leetcode;
+
+public class FinalValueOfVariableAfterPerformingOperations {
+    public static void main(String[] args) {
+        String[] operations={"--X","X++","X++"};
+        int i = finalValueAfterOperations(operations);
+        System.out.println(i);
+    }
+
+    private static int finalValueAfterOperations(String[] operations) {
+        int res=0;
+
+        return res;
+    }
+}
