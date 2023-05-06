a7dac4fd525a0c6cde6585909f4addd2a6b335f3
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-06 16:58:15 +0530

    ThreeDivisors

---
+++ src/main/java/com/bootcoding/leetcode/ThreeDivisors.java
@@ -0,0 +1,20 @@
+package com.bootcoding.leetcode;
+
+public class ThreeDivisors
+{
+    public static void main(String[] args) {
+        int n=14;
+        boolean res = isThree(n);
+        System.out.println(res);
+    }
+
+    private static boolean isThree(int n) {
+        int count=0;
+        for (int i = 1; i <=n; i++) {
+            if (n%i==0){
+                count++;
+            }
+        }
+        return count==3;
+    }
+}
