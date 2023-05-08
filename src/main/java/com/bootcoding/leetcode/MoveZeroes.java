c3d1d8dbb8133b49db2ab2ad18ee2f9432f560c4
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-08 11:12:37 +0530

    MoveZeroes

--- src/main/java/com/bootcoding/leetcode/MoveZeroes.java
+++ src/main/java/com/bootcoding/leetcode/MoveZeroes.java
@@ -9,16 +9,15 @@ public class MoveZeroes
         MoveZeroes mm = new MoveZeroes();
         mm.moveZeroes(nums);
     }
+
     public void moveZeroes(int[] nums) {
         int n = nums.length;
         int indexCount = 0;
-        for (int index = 0; index < nums.length; index++)
-        {
+        for (int index = 0; index < nums.length; index++) {
             if(nums[index] != 0){
                 nums[indexCount++] = nums[index];
             }
         }
-
         while(indexCount < n)
         {
             nums[indexCount++] = 0;
