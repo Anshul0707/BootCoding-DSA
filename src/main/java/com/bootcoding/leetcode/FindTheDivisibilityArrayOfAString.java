96881d15f3e7fc942e288543ca0753ee3ee324a6
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-08 11:10:54 +0530

    FindTheDivisibilityArrayOfAString

--- src/main/java/com/bootcoding/leetcode/FindTheDivisibilityArrayOfAString.java
+++ src/main/java/com/bootcoding/leetcode/FindTheDivisibilityArrayOfAString.java
@@ -15,11 +15,13 @@ public class FindTheDivisibilityArrayOfAString
         int n = word.length();
         int[] div = new int[n];
         int num = 0;
-        System.out.println("gggggggggggggg== "+word.charAt(0));
+
         for (int i = 0; i < n; i++) {
             num = (num * 10 + (word.charAt(i) - '0')) % m;
             if (num == 0) {
-                div[i] = 1;
+                for (int j = 0; j <= i; j++) {
+                    div[j] = 1;
+                }
             }
         }
         return div;
