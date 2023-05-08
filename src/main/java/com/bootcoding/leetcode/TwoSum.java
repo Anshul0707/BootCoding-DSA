ddf34910de9d71fb794d377c22223323e7f8f1de
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-08 11:11:20 +0530

    TwoSum

--- src/main/java/com/bootcoding/leetcode/TwoSum.java
+++ src/main/java/com/bootcoding/leetcode/TwoSum.java
@@ -5,8 +5,7 @@ import java.util.Map;
 
 public class TwoSum
 {
-    public int[] twoSum(int[] nums, int target)
-    {
+    public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length; i++)
         {
             for (int j = i + 1; j < nums.length; j++)
