2700b57645b5f9cb1d809c0b7221252b830f384a
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-09 13:46:13 +0530

    MaximumCount

--- src/main/java/com/bootcoding/leetcode/MaximumCount.java
+++ src/main/java/com/bootcoding/leetcode/MaximumCount.java
@@ -6,20 +6,16 @@ public class MaximumCount
         int pCount=0;
         int nCount=0;
         int maximumCount=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if (nums[i]<0)
-            {
+        for(int i=0;i<nums.length;i++) {
+            if (nums[i]<0) {
                 nCount++;
             }
-            else if(nums[i]>0)
-            {
+            else if(nums[i]>0) {
                 pCount++;
             }
         }
 
-        if (pCount>nCount)
-        {
+        if (pCount>nCount) {
             maximumCount=pCount;
         }
         else {
