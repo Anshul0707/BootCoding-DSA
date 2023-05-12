e9be379ee8c120dc05655b43dcef962220446a3b
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-12 11:07:01 +0530

    sumOfNumbers

--- src/main/java/com/bootcoding/recursion/SumOfNumbers.java
+++ src/main/java/com/bootcoding/recursion/SumOfNumbers.java
@@ -2,16 +2,13 @@ package com.bootcoding.recursion;
 
 public class SumOfNumbers
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int num = 6;
         System.out.println(sumOfNumbers(num));
     }
 
-    private static int sumOfNumbers(int num)
-    {
-        if (num <= 1)
-        {
+    private static int sumOfNumbers(int num) {
+        if (num <= 1) {
             return num;
         }
         return num + sumOfNumbers(num - 1);
