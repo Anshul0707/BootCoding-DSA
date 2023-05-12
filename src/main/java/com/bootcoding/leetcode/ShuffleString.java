3479349ce2f381b81d8c0e374182f07e47aeb99d
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-12 11:06:30 +0530

    ShuffleString

--- src/main/java/com/bootcoding/leetcode/ShuffleString.java
+++ src/main/java/com/bootcoding/leetcode/ShuffleString.java
@@ -5,6 +5,7 @@ public class ShuffleString
     public static void main(String[] args) {
         String s="codeleet";
         int[] indices={4,5,6,7,0,2,1,3};
+
         System.out.println(restoreString(s, indices));
     }
 
