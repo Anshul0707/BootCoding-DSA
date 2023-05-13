f2a93cf13814c45aed4d2c23b408f3a3d7d47866
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-13 19:33:45 +0530

    FindTheIndexOfTheFirstOccurrenceInAString

--- src/main/java/com/bootcoding/leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/main/java/com/bootcoding/leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -2,6 +2,7 @@ package com.bootcoding.leetcode;
 
 public class FindTheIndexOfTheFirstOccurrenceInAString {
     public static void main(String[] args) {
-
+        String haystack="sadbutsad";
+        String needle="sad";
     }
 }
