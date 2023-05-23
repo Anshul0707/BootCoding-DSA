1c53126c4714241faa4739b6332550d7ddf00c82
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-23 22:10:41 +0530

    EvenOdd

---
+++ src/main/java/com/bootcoding/Practice/EvenOdd.java
@@ -0,0 +1,40 @@
+package com.bootcoding.Practice;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the size of the array
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        // Create the array
+        int[] array = new int[size];
+
+        // Populate the array
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        // Check even/odd for each element
+        for (int i = 0; i < size; i++) {
+            if (isEven(array[i])) {
+                System.out.println(array[i] + " is even.");
+            } else {
+                System.out.println(array[i] + " is odd.");
+            }
+        }
+
+        scanner.close();
+    }
+
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
+
