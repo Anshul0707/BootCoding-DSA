eb99d0ab750af18a693a4f7ff5e1200c60c6c852
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-24 23:25:35 +0530

    ..

--- src/main/java/com/bootcoding/leetcode/ConcatenationOfArray.java
+++ src/main/java/com/bootcoding/leetcode/ConcatenationOfArray.java
@@ -9,10 +9,9 @@ public class ConcatenationOfArray
             System.out.print(res);
         }
     }
-
     private static int[] getConcatenation(int[] nums) {
         int[] ans = new int[(nums.length)*2];
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++){
             ans[i] = nums[i];
             ans[i + nums.length] = nums[i];
         }
