8b4cdf70934102aebf35455cb9c2fbef1bd86b93
Anshul0707 <anshul.markwade786@gmail.com>
2023-05-24 23:27:00 +0530

    ..

---
+++ src/main/java/com/bootcoding/Practice/unicode.java
@@ -0,0 +1,15 @@
+package com.bootcoding.Practice;
+
+public class unicode {
+
+
+        public static void main(String[] args) {
+            System.out.println("INDIA");
+            System.out.println("INDIA is known as Bharat in hindi");
+            System.out.println("\u092D\u0964\u0931\u0924");
+
+
+
+        }
+    }
+
