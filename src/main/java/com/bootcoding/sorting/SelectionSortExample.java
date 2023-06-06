a277c0ac9b30e13c41feafa1a1cc557159637780
Anshul0707 <anshul.markwade786@gmail.com>
2023-06-06 11:22:53 +0530

    ..

--- src/main/java/com/bootcoding/sorting/SelectionSortExample.java
+++ src/main/java/com/bootcoding/sorting/SelectionSortExample.java
@@ -1,9 +1,7 @@
 package com.bootcoding.sorting;
 
-public class SelectionSortExample
-{
-    public static void main(String[] args)
-    {
+public class SelectionSortExample {
+    public static void main(String[] args) {
         int[] arr = {12, 11, 13, 5, 6, 7};
         System.out.println("Before Sorting...");
         for (int a : arr) {
@@ -11,21 +9,16 @@ public class SelectionSortExample
         }
         System.out.println("\nAfter Sorting...");
         selectionSort(arr);
-        for (int i : arr)
-        {
+        for (int i : arr) {
             System.out.print(i + "  ");
         }
     }
 
-    private static void selectionSort(int[] arr)
-    {
-        for (int i = 0; i < arr.length - 1; i++)
-        {
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++)
-            {
-                if (arr[j] < arr[index])
-                {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
                     index = j;
                 }
             }
