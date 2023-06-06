0e1de70ddbdc2a8ea4e95933b043ce646c883bd2
Anshul0707 <anshul.markwade786@gmail.com>
2023-06-06 11:24:23 +0530

    ..

--- src/main/java/com/bootcoding/recursion/ReverseString.java
+++ src/main/java/com/bootcoding/recursion/ReverseString.java
@@ -1,29 +1,25 @@
 package com.bootcoding.recursion;
 
-public class ReverseString
-{
-    public static void main(String[] args)
-    {
-        int arr[]={1,4,6,8};
-        reverseString(arr,0);
-        for (int i=0;i<arr.length;i++) {
+public class ReverseString {
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 6, 8};
+        reverseString(arr, 0);
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
 
     }
 
-    private static void reverseString(int arr[],int i)
-    {
-         int n=arr.length;
-         if (i>n/2)
-         {
-             return;
-         }
-             int t=arr[1];
-             arr[i]=arr[n-1-i];
-             arr[n-1-i]=t;
+    private static void reverseString(int arr[], int i) {
+        int n = arr.length;
+        if (i > n / 2) {
+            return;
+        }
+        int t = arr[1];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = t;
 
-             reverseString(arr,i+1);
+        reverseString(arr, i + 1);
     }
 
 }
